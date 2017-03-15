8cfe3473754d0f50eaf0f2924355bf4c9402539c
Christian Wlatschiha <c.wlatschiha@gmail.com>
2017-03-15 23:45:12 +0100

    jest-sonar-reporter-4 Replace TestExecutions class with function.

--- index.js
+++ index.js
@@ -3,15 +3,14 @@
 const fs = require('fs')
 const path = require('path')
 const xml = require('xml')
-const TestExecutions = require('./lib/TestExecutions')
+const testExecutions = require('./lib/testExecutions')
 
 const XML_INDENT = '  '
 const BASE = 'test-report.xml'
 const out = process.env.TEST_REPORT_PATH || process.cwd();
 
 module.exports = (results) => {
-  const testExecutions = new TestExecutions(results)
-  const report = xml(testExecutions, {declaration: true, indent: XML_INDENT})
+  const report = xml(testExecutions(results), {declaration: true, indent: XML_INDENT})
 
   if (process.env.TEST_REPORT_PATH && !fs.existsSync(out)) {
     fs.mkdirSync(out)
